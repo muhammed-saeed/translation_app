23eac6ef3f5b792ef22f97d47661a48d3ec1972e
amohamed <amohamed@malomatia.com>
2022-09-03 01:43:04 +0200

    create note context and service

---
+++ src/context/notes.context.jsx
@@ -0,0 +1,58 @@
+import React, { createContext, useContext, useMemo, useState } from "react";
+import { translateNote } from "../services/translation";
+
+const NoteFormContext = createContext({});
+
+export const NoteFormProvider = ({ children }) => {
+  const [note, setNote] = useState({
+    title: "",
+    details: "",
+    category: "money",
+  });
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+
+  const noteFormChanged = (name, value) => {
+    setNote((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+    console.log("Are you here");
+    if (note.title == "") {
+      setTitleError(true);
+    }
+    if (note.details == "") {
+      setDetailsError(true);
+    }
+    if (note.title && note.details) {
+      await translateNote(note);
+    }
+  };
+
+  const memoedValue = useMemo(
+    () => ({
+      note,
+      titleError,
+      detailsError,
+      noteFormChanged,
+      handleSubmit,
+    }),
+    [note]
+  );
+  return (
+    <NoteFormContext.Provider value={memoedValue}>
+      {children}
+    </NoteFormContext.Provider>
+  );
+};
+
+export default function useNoteForm() {
+  return useContext(NoteFormContext);
+}
